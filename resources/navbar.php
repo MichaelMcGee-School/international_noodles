714039253248da0f44c6f36f6956f75f37609738
Mike McGee <mike.mcgee@my.simpson.edu>
2019-10-22 15:21:26 -0500

    fixed filename

---
+++ resources/navbar.php
@@ -0,0 +1,28 @@
+<nav>
+    <ul>
+        <li>
+            <a href="../public.html/index.php">Home</a>
+        </li>
+        <li>
+            <a href="../public.html/interactive_map_page.php">Interactive Map</a>
+        </li>
+        <li>
+            <a href="../public.html/rankings_page.php">Rankings</a>
+        </li>
+        <li>
+            <a href="../public.html/recipes_page.php">Recipes</a>
+        </li>
+        <li>
+            <a href="../public.html/fun_facts_page.php">Fun Facts</a>
+        </li>
+        <li>
+            <a href="../public.html/about_page.php">About Page</a>
+        </li>
+        <li>
+            <a href="../public.html/contact_page.php">Contact Us</a>
+        </li>
+        <li>
+            <a href="../public.html/signup.php">Sign Up</a>
+        </li>
+    </ul>
+</nav>
