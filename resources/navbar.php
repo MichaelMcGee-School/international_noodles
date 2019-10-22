7c7589539a6e8211af5ef6aea16c60fbda8f37fa
Mike McGee <mike.mcgee@my.simpson.edu>
2019-10-22 15:23:48 -0500

    changed navbar links

--- resources/navbar.php
+++ resources/navbar.php
@@ -1,28 +1,28 @@
 <nav>
     <ul>
         <li>
-            <a href="../public.html/index.php">Home</a>
+            <a href="index.php">Home</a>
         </li>
         <li>
-            <a href="../public.html/interactive_map_page.php">Interactive Map</a>
+            <a href="interactive_map_page.php">Interactive Map</a>
         </li>
         <li>
-            <a href="../public.html/rankings_page.php">Rankings</a>
+            <a href="rankings_page.php">Rankings</a>
         </li>
         <li>
-            <a href="../public.html/recipes_page.php">Recipes</a>
+            <a href="recipes_page.php">Recipes</a>
         </li>
         <li>
-            <a href="../public.html/fun_facts_page.php">Fun Facts</a>
+            <a href="fun_facts_page.php">Fun Facts</a>
         </li>
         <li>
-            <a href="../public.html/about_page.php">About Page</a>
+            <a href="about_page.php">About Page</a>
         </li>
         <li>
-            <a href="../public.html/contact_page.php">Contact Us</a>
+            <a href="contact_page.php">Contact Us</a>
         </li>
         <li>
-            <a href="../public.html/signup.php">Sign Up</a>
+            <a href="signup.php">Sign Up</a>
         </li>
     </ul>
 </nav>
